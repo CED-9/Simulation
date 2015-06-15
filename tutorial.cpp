3f0c7dde2e5820e85aff254c68e4d962e5e0cfd0
CED-9 <liujunming@liujunmingdeMacBook-Pro.local>
2015-06-14 21:07:45 -0700

    test

--- tutorial.cpp
+++ tutorial.cpp
@@ -53,57 +53,71 @@ struct PlayerInfo {
     int features[10];
 };
 
-void writePayoff (CreditNet* creditNet, double unitTransactionPayoff) {
+void writePayoff (PlayerInfo [] players, int playerNum, double unitTransactionPayoff) {
     
-    for (int i = 0; i < creditNet->finNum; ++i) {
-//        cout << this->finAgent[i]->getNodeID() << ": "
-//        << "Transactions " << this->finAgent[i]->transactionNum << ", Current Banlance "
-//        << this->finAgent[i]->getCurrBanlance() << endl;
-        
-        
-        
-    }
-    // ---- create from scratch ----
     
-    // document is the root of a json message
-    rapidjson::Document fromScratch;
+//    rapidjson::Document result;
+//    result.SetObject();
+//    rapidjson::Value playerArray(rapidjson::kArrayType);
+//    rapidjson::Document::AllocatorType& allocator = result.GetAllocator();
+//    for (int i = 0; i < playerNum; ++i) {
+//        array.PushBack("hello", allocator).PushBack("world", allocator);
+//        
+//        // create a rapidjson object type
+//        rapidjson::Value object(rapidjson::kObjectType);
+//        object.AddMember("roll", "All", allocator);
+//        object.AddMember("strategy", players[i].strategy, allocator);
+//        object.AddMember("payoff", players.payoff, allocator);
+//        
+//        
+//        fromScratch.AddMember("object", object, allocator);
+//        fromScratch["object"]["hello"] = "world";
+//        
+//    }
+ 
     
-    // define the document as an object rather than an array
-    fromScratch.SetObject();
     
-    // create a rapidjson array type with similar syntax to std::vector
-    rapidjson::Value array(rapidjson::kArrayType);
-    
-    // must pass an allocator when the object may need to allocate memory
-    rapidjson::Document::AllocatorType& allocator = fromScratch.GetAllocator();
-    
-    // chain methods as rapidjson provides a fluent interface when modifying its objects
-    array.PushBack("hello", allocator).PushBack("world", allocator);
-    
-    fromScratch.AddMember("hello", "world", allocator);
-    fromScratch.AddMember("number", 2, allocator);
-    fromScratch.AddMember("array", array, allocator);
-    
-    // create a rapidjson object type
-    rapidjson::Value object(rapidjson::kObjectType);
-    object.AddMember("hello", "world", allocator);
-    fromScratch.AddMember("object", object, allocator);
-    fromScratch["object"]["hello"] = "world";
-    
-    output(fromScratch);
-    
-    // ---- parse from string ----
-    
-    // Convert JSON document to string
-    rapidjson::StringBuffer strbuf;
-    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
-    fromScratch.Accept(writer);
-    
-    // parse json string
-    rapidjson::Document parsedFromString;
-    parsedFromString.Parse<0>(strbuf.GetString());
-    
-    output(parsedFromString);
+//    // ---- create from scratch ----
+//    
+//    // document is the root of a json message
+//    rapidjson::Document fromScratch;
+//    
+//    // define the document as an object rather than an array
+//    fromScratch.SetObject();
+//    
+//    // create a rapidjson array type with similar syntax to std::vector
+//    rapidjson::Value array(rapidjson::kArrayType);
+//    
+//    // must pass an allocator when the object may need to allocate memory
+//    rapidjson::Document::AllocatorType& allocator = fromScratch.GetAllocator();
+//    
+//    // chain methods as rapidjson provides a fluent interface when modifying its objects
+//    array.PushBack("hello", allocator).PushBack("world", allocator);
+//    
+//    fromScratch.AddMember("hello", "world", allocator);
+//    fromScratch.AddMember("number", 2, allocator);
+//    fromScratch.AddMember("array", array, allocator);
+//    
+//    // create a rapidjson object type
+//    rapidjson::Value object(rapidjson::kObjectType);
+//    object.AddMember("hello", "world", allocator);
+//    fromScratch.AddMember("object", object, allocator);
+//    fromScratch["object"]["hello"] = "world";
+//    
+//    output(fromScratch);
+//    
+//    // ---- parse from string ----
+//    
+//    // Convert JSON document to string
+//    rapidjson::StringBuffer strbuf;
+//    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+//    fromScratch.Accept(writer);
+//    
+//    // parse json string
+//    rapidjson::Document parsedFromString;
+//    parsedFromString.Parse<0>(strbuf.GetString());
+//    
+//    output(parsedFromString);
 }
 
 
@@ -131,7 +145,7 @@ int main(int, char*[]) {
     
     // create a rapidjson object type
     rapidjson::Value object(rapidjson::kObjectType);
-    object.AddMember("hello", "world", allocator);
+    object.AddMember("hello", NULL, allocator);
     fromScratch.AddMember("object", object, allocator);
     fromScratch["object"]["hello"] = "world";
     

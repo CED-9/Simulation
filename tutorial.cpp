1f9105f19ae451a593ac8e41c1a252411ade2aa8
CED-9 <liujunming@liujunmingdeMacBook-Pro.local>
2015-06-14 22:39:02 -0700

    debug

--- tutorial.cpp
+++ tutorial.cpp
@@ -66,7 +66,7 @@ void writePayoff (std::vector<PlayerInfo> &players) {
         object.SetObject();
         object.AddMember("roll", "All", allocator);
         object.AddMember("strategy", players[i].strategy, allocator);
-        object.AddMember("payoff", players.payoff, allocator);
+        object.AddMember("payoff", players[i].payoff, allocator);
         playerArray.PushBack(object, allocator);
     }
     
